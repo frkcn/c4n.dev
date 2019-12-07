68ef882169efe695ad4d883197a3e9e2037508ec
Faruk Can <faruk.can@tarfin.com>
2019-12-07 23:54:13 +0300

    building personal website

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="py-8 lg:py-16 px-6 md:px-16 lg:px-24">
+    <Header />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="lg:pl-32 mt-12">
+    <div class="max-w-md">
+      <h1 class="text-2xl font-extrabold text-black mb-4">
+        Who am I?
+      </h1>
+      <div class="text-lg text-grey-darkest leading-normal spaced-y-6">
+        <p>I'm Faruk, a full-stack developer and engineering team lead <a href="https://twitter.com/tarfincom" class="text-black font-bold">@tarfin</a>.</p>
+        <p>Follow me on twitter <a class="text-black font-bold" href="https://twitter.com/frkcn">@frkcn</a>.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
