cdaa60447e8e882019a0c1c0c918414e9c1792fd
Faruk Can <faruk.can@tarfin.com>
2020-02-12 11:37:43 +0300

    some ui improvements

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,19 @@
     </h1>
     <div class="text-lg text-grey-darkest leading-normal">
       <p class="mb-6">
-        I'm Faruk from Istanbul, Turkey where I work as a Head of Engineering at <a href="https://twitter.com/tarfincom" class="text-black font-bold underline">@tarfin</a>.
+        I'm Faruk from Istanbul, Turkey where I work as a Head of Engineering at
+        <a href="https://twitter.com/tarfincom" class="font-semibold work"><span class="h">@tarfin</span></a>.
         I have 10 years of experience with web development with a focus on API, monolith and cloud infrastructure.
       </p>
       <p class="mb-6">
         In my spare time, I enjoys writing calligraphy and discovering new technologies.
       </p>
       <p class="mb-6">
-        Follow me on twitter <a class="text-black font-bold underline" href="https://twitter.com/frkcn">@frkcn</a>.
+        Follow me on twitter <a href="https://twitter.com/frkcn" class="font-semibold personal"><span class="h">@frkcn</span></a>.
       </p>
       <p>
-        Also check my open source projects <a href="https://github.com/frkcn" class="text-black font-bold underline">@github/frkcn</a>
-        or my team's open source projects <a href="https://github.com/tarfin-labs" class="text-black font-bold underline">@github/tarfin-labs</a>
+        Also check my open source projects <a href="https://github.com/frkcn" class="font-semibold personal"><span class="h">@github/frkcn</span></a>
+        or my team's open source projects <a href="https://github.com/tarfin-labs" class="font-semibold work"><span class="h">@github/tarfin-labs</span></a>
       </p>
     </div>
   </div>
